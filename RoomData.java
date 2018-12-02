453cd2217c579d02a577834831607a9d2c510f09
nhatle0296 <nhatle0296@gmail.com>
2018-12-02 12:24:51 -0800

    Add files via upload

---
+++ RoomData.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class RoomData implements Subject{
+	private ArrayList<Observer> observers;
+	public RoomData() {
+		observers = new ArrayList<Observer>();
+	}
+	
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
+	
+	public void removeObserver(Observer o) {
+		int j = observers.indexOf(o);
+		if (j >= 0) {
+			observers.remove(j);
+		}
+	}
+	
+	public void notifyObservers() {
+		for (int j = 0; j < observers.size(); j++) {
+			Observer observer = (Observer) observers.get(j);
+			observer.update();
+		}
+	}
+	
+	public void measurementsChanged() {
+		notifyObservers();
+	}
+}
