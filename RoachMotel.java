2b25dd651aeb961d26178e788abd29206562302d
Kohvu <44853020+Kohvu@users.noreply.github.com>
2018-12-03 00:44:02 -0800

    Update RoachMotel.java

--- RoachMotel.java
+++ RoachMotel.java
@@ -10,5 +10,17 @@ public class RoachMotel {
         }
         return roachMotel;
     }
+    
+    public void createRooms() {
+        //Generate 5 rooms
+        MotelRoomFactory factory = new MotelRoomFactory();
+        MotelRoomService service = new MotelRoomService(factory);
+
+        MotelRoom reg1 = service.setRoom("regular");
+        MotelRoom reg2 = service.setRoom("regular");
+        MotelRoom del1 = service.setRoom("deluxe");
+        MotelRoom del2 = service.setRoom("deluxe");
+        MotelRoom sui1 = service.setRoom("suite");
+    }
 }
 
