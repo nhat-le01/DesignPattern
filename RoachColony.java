d5b386270b0c3d9f67b3d666f14f0136c8c28677
Kohvu <44853020+Kohvu@users.noreply.github.com>
2018-12-09 16:20:47 -0800

    Add files via upload

--- RoachColony.java
+++ RoachColony.java
@@ -1,4 +1,17 @@
 
 public class RoachColony {
-
+	public String name;
+	public int initialPopulation;
+	public double growthRate;
+	
+	public RoachColony() {
+		name = "Default";
+		initialPopulation = 1;
+		growthRate = 1.1;
+	}
+	public RoachColony(String name, int initialPopulation, double growthRate) {
+		this.name = name;
+		this.initialPopulation = initialPopulation;
+		this.growthRate = growthRate;
+	}
 }
